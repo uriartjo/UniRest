00f7e9c6658a068419b2bc702555ed47fe343498
Joey Uriarte <Joey.Uriarte@saintandrews.net>
2022-04-01 08:51:50 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	TwilioAPI.makeAPICall();
+
+    /*
+    Resources:
+        1. https://www.baeldung.com/unirest
+        2. http://kong.github.io/unirest-java/#requests
+     */
+    }
+}
---
+++ src/com/company/TwilioAPI.java
@@ -0,0 +1,12 @@
+package com.company;
+
+import kong.unirest.HttpResponse;
+import kong.unirest.JsonNode;
+import kong.unirest.Unirest;
+
+public class TwilioAPI {
+
+    public static void makeAPICall() {
+
+    }
+}
